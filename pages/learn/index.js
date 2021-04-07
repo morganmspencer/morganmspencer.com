40ebb1f7add43a47cc96e13e6ab797459cc59330
Morgan Spencer <morgan@spencercreative.co>
2021-04-07 16:14:10 -0400

    Restructure pages

---
+++ pages/learn/index.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import { getPosts } from 'pages/api/posts'
+import MetaHead from 'components/MetaHead'
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <MetaHead />
+      <ul className="min-h-screen">
+        {posts.map(post => (
+          <li key={post.slug}>
+            <Link href={`/${post.category}/${post.slug}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = getPosts()
+
+  return {
+    props: { posts },
+  }
+}
