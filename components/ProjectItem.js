01e3c0df2965601d043ca376bed45d92b7ed73aa
Morgan Spencer <morgan@spencercreative.co>
2021-04-08 16:57:34 -0400

    Add image to project item

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -2,11 +2,12 @@ import Link from 'next/link'
 
 export default function ProjectItem({ project }) {
   return (
-    <Link href={`/porfolio/${project.slug}`}>
+    <Link href={`/portfolio/${project.slug}`}>
       <a className="block aspect-w-16 aspect-h-9 group" style={{backgroundColor: project.color}}>
-        <div className="w-full h-full p-4 transition-all md:p-6 group-hover:p-2 group-focus:p-2">
+        <div className="w-full h-full p-4 transition-all duration-500 md:p-6 group-hover:p-2 group-focus:p-2">
           <div className="w-full h-full relative bg-light flex items-center justify-center">
-            <h3 className="opacity-0 transition-opacity group-hover:opacity-100 group-focus:opacity-100">{project.title}</h3>
+            <div className="absolute top-0 left-0 w-full h-full bg-cover duration-500 group-hover:opacity-5 group-focus:opacity-5" style={{backgroundImage: `url(${project.thumbnail})`}} />
+            <h3 className="opacity-0 transition-opacity duration-500 group-hover:opacity-100 group-focus:opacity-100">{project.title}</h3>
           </div>
         </div>
       </a>
