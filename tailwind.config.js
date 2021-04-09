b3137cc73ba947290e072d55e3929e584c319f38
Morgan Spencer <morgan@spencercreative.co>
2021-04-08 22:49:56 -0400

    Update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      white: '#ffffff',
-      black: '#000000',
-      transparent: 'transparent',
-      dark: '#22223B',
-      navy: '#4A4E69',
-      lavender: '#9A8C98',
-      tan: '#C9ADA7',
-      light: '#F2E9E4',
-    },
     fontFamily: {
       body: ['"Futura PT"', ...defaultTheme.fontFamily.sans],
     },
     extend: {
+      colors: {
+        dark: '#22223B',
+        navy: '#4A4E69',
+        lavender: '#9A8C98',
+        tan: '#C9ADA7',
+        light: '#F2E9E4',
+      },
       margin: {
         '-wrap': '-5.62vw',
         'wrap': '5.62vw',
