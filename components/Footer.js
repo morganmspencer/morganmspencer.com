860830f0b3934c7c027fb7c2bc30385c89a32497
Morgan Spencer <morgan@spencercreative.co>
2021-04-09 16:01:10 -0400

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ const blog = require('nmbs.config.json')
 
 export default function Footer() {
   return (
-    <footer role="contentinfo" className="text-center text-xs pb-1">
+    <footer role="contentinfo" className="text-center text-xs pb-1 absolute bottom-0 w-full">
       <p>Copyright &copy; {new Date().getFullYear()} <Link href="/">{blog.name}</Link>. All rights reserved.</p>
     </footer>
   )
